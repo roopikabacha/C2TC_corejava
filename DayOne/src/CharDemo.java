83d2761d51807f022cf0d2ac45bbdb9d6136f217
ROOPIKA <ROOPIKA@DESKTOP-RCJSP93>
2023-04-17 14:56:48 +0530

    first commit

--- DayOne/src/CharDemo.java
+++ DayOne/src/CharDemo.java
@@ -1,4 +1,14 @@
 
 public class CharDemo {
 
+	public static void main(String[] args) {  
+		  
+        char char1='a';  
+        char char2='A';  
+          
+  
+        System.out.println("char1: "+char1);  
+        System.out.println("char2: "+char2);                  
+    }         
+
 }
